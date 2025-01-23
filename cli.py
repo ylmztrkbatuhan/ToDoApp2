09de1783969f7b8aa3a16fefd0f1a89485bbf6a5
oboy21 <oboy11b@gmail.com>
2025-01-23 11:55:27 +0300

    Create basic gui without interactions

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
-from modules.functions import get_todos, write_todos
+#from modules.functions import get_todos, write_todos
+from modules import functions
 import time
 
 now = time.strftime("%b %d, %Y %H:%M:%S")
@@ -10,15 +11,15 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos(todos, "todos.txt")
+        functions.write_todos(todos, "todos.txt")
 
     elif user_action.startswith('show'):
 
-        todos = get_todos("todos.txt")
+        todos = functions.get_todos("todos.txt")
 
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
@@ -34,12 +35,12 @@ while True:
 
             number = number - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos)
+            functions.write_todos(todos)
         except ValueError:
             print("Your command is not valid")
             continue
@@ -48,12 +49,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
